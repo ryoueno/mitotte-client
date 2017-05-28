fde7c1c678eef4aa2ea32a35f77e08f8d90ec3a9
uenoryo <r.r.ag0611@outlook.jp>
2017-05-29 08:41:14 +0900

    The tasks for capture screen are now repeatable.

--- mitotte-client/AppDelegate.swift
+++ mitotte-client/AppDelegate.swift
@@ -11,10 +11,21 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let ACTIVITY_TOLERANCE = 1
+    let ACTIVITY_INTERVAL = 2
+    let ACTIVITY_WAITTIME = 5
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let activity = NSBackgroundActivityScheduler(identifier: Bundle.main.bundleIdentifier!)
+        activity.repeats = true
+        activity.tolerance = TimeInterval(ACTIVITY_TOLERANCE)
+        activity.interval = TimeInterval(ACTIVITY_WAITTIME)
+
+        activity.schedule() { (completion: NSBackgroundActivityCompletionHandler) in
+            sleep(UInt32(self.ACTIVITY_WAITTIME))
+            print("execute")
+            completion(NSBackgroundActivityScheduler.Result.finished)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
