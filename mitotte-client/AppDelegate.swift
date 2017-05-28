3cc0dfd6c5617dcda3af03b6d27ae4af43772c79
uenoryo <r.r.ag0611@outlook.jp>
2017-05-29 08:08:37 +0900

    first commit.

---
+++ mitotte-client/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mitotte-client
+//
+//  Created by 上野 涼 on 2017/05/29.
+//  Copyright © 2017年 上野 涼. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mitotte-client/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mitotte-client
+//
+//  Created by 上野 涼 on 2017/05/29.
+//  Copyright © 2017年 上野 涼. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
