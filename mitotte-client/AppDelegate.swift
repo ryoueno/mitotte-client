1b7f31524811677ae317a201f7adc409db590919
uenoryo <r.r.ag0611@outlook.jp>
2017-05-29 14:30:17 +0900

    Implement function that sending screenshot regularly.

--- mitotte-client/AppDelegate.swift
+++ mitotte-client/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import Alamofire
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,15 +16,44 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let ACTIVITY_INTERVAL = 2
     let ACTIVITY_WAITTIME = 5
 
+    let SCREENSHOT_FILENAME = "screenshot.png"
+    let SCREENSHOT_UPLOAD_URL = ProcessInfo.processInfo.environment["POST_SCREENSHOT_URL"]!
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let activity = NSBackgroundActivityScheduler(identifier: Bundle.main.bundleIdentifier!)
+        let parameters = ["uuid": NSUUID().uuidString]
         activity.repeats = true
         activity.tolerance = TimeInterval(ACTIVITY_TOLERANCE)
         activity.interval = TimeInterval(ACTIVITY_WAITTIME)
 
         activity.schedule() { (completion: NSBackgroundActivityCompletionHandler) in
             sleep(UInt32(self.ACTIVITY_WAITTIME))
-            print("execute")
+
+            let imageRep: CGImage = CGDisplayCreateImage(CGMainDisplayID())!
+            let bitmap = NSBitmapImageRep(cgImage: imageRep)
+            let properties = NSDictionary() as! [String : AnyObject]
+            let data = bitmap.representation(using: NSPNGFileType, properties: properties)
+            Alamofire.upload(
+                multipartFormData: { multipartFormData in
+                    multipartFormData.append(data!, withName: "screenshot", fileName: self.SCREENSHOT_FILENAME, mimeType: "image/png")
+                    for (key, val) in parameters {
+                        multipartFormData.append(val.data(using: String.Encoding.utf8)!, withName: key)
+                    }
+                },
+                to: self.SCREENSHOT_UPLOAD_URL,
+                encodingCompletion: { encodingResult in
+                    switch encodingResult {
+                    case .success(let upload, _, _):
+                        upload.responseJSON { response in
+                            if let jsonResponse = response.result.value as? [String: Any] {
+                                print(jsonResponse)
+                            }
+                        }
+                    case .failure(let encodingError):
+                        print(encodingError)
+                    }
+                }
+            )
             completion(NSBackgroundActivityScheduler.Result.finished)
         }
     }
